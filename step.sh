a545366bbd7bfc9d56c02ca34d662a3d8b4ba56e
Fabio Gallonetto <fabio@futureworkshops.com>
2015-12-11 12:23:23 +0100

    First version

---
+++ step.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+AWS_ACCESS_KEY_ID=$aws_access_key
+AWS_SECRET_ACCESS_KEY="$aws_secret_access_key"
+aws s3 cp s3://$S3_BUCKET/$S3_PATH $output_location 
+
+# --- Export Environment Variables for other Steps:
+# You can export Environment Variables for other Steps with
+#  envman, which is automatically installed by `bitrise setup`.
+# A very simple example:
+#  envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
+# Envman can handle piped inputs, which is useful if the text you want to
+# share is complex and you don't want to deal with proper bash escaping:
+#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
+# You can find more usage examples on envman's GitHub page
+#  at: https://github.com/bitrise-io/envman
+
+#
+# --- Exit codes:
+# The exit code of your Step is very important. If you return
+#  with a 0 exit code `bitrise` will register your Step as "successful".
+# Any non zero exit code will be registered as "failed" by `bitrise`.
